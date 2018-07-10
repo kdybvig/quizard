eb9bc7fec78f04f77582e13eb4ee4c9ce2f7bbd5
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-07-10 15:01:29 -0500

    removed unneccesary files, changed app name

---
+++ src/Game.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Game.css';
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="Game">
+        <h1>Team Review Game</h1>
+      </div>
+    );
+  }
+}
+
+export default Game;
