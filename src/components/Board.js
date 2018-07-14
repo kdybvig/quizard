f98aa6d7add8de07ab65e959a6aa9bfdd482890e
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-07-14 11:48:01 -0500

    added boxes for points

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,16 +2,22 @@ import React from 'react';
 
 
 const Board = ({title, categories}) => {
+  const arr = [];
+  for(let i = 1; i< 26; i++) {
+    const value = Math.ceil(i/5)*100
+    arr.push(<p className='points'>{value}</p>)
+  }
+
+  const categoryBoxes = categories.map((qq, index) => {
+    const key = `q-${index}`
+    return <p className='category' key={key}>{qq}</p>
+  });
   return (
     <div id='board'>
       <h1>{title}</h1>
-      <div id='categories'>
-      {
-        categories.map((qq, index) => {
-          const key = `q-${index}`
-          return <p key={key}>{qq}</p>
-        })
-      }
+      <div id='boxes'>
+        {categoryBoxes}
+        {arr}
       </div>
     </div>
   )
