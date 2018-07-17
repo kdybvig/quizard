685eeb997a51180990f56b76861dba8091d3224d
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-07-17 09:40:53 -0500

    modified styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Home = (props) => {
   return (
-    <div>
+    <div id='home'>
       <h1>QuizKnow</h1>
       <h4><Link to='/load'>Load an existing quiz</Link></h4>
       <h4><Link to='/create'>Create a new quiz</Link></h4>
