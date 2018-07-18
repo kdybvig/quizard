c1443b5ec125ea3551d2a6184a268697c84289e8
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-07-18 13:41:00 -0500

    updated routes to use public url

--- src/components/RouterPage.js
+++ src/components/RouterPage.js
@@ -21,14 +21,14 @@ const RouterPage = (props) => {
     <Router>
       <div className='game'>
         <nav><Link to={process.env.PUBLIC_URL + '/'}><i className="fas fa-home" onClick={()=>props.dispatch(restartGame())}></i></Link></nav>
-        <Route exact path="/" component={Home}/>
-        <Route path="/create" component={CategoryForm}/>
-        <Route path="/load" component={LoadQuizzes}/>
-        <Route path="/createquestions" component={QuestionForm}/>
-        <Route path="/addteams" component={TeamForm}/>
-        <Route path="/game" component={TeamDisplay}/>
-        <Route path="/game/board" component={BoardContainer}/>
-        <Route path="/game/question" component={QuestionDisplay}/>
+        <Route exact path={process.env.PUBLIC_URL + "/"} component={Home}/>
+        <Route path={process.env.PUBLIC_URL + "/create"} component={CategoryForm}/>
+        <Route path={process.env.PUBLIC_URL + "/load"} component={LoadQuizzes}/>
+        <Route path={process.env.PUBLIC_URL + "/createquestions"} component={QuestionForm}/>
+        <Route path={process.env.PUBLIC_URL + "/addteams"} component={TeamForm}/>
+        <Route path={process.env.PUBLIC_URL + "/game"} component={TeamDisplay}/>
+        <Route path={process.env.PUBLIC_URL + "/game/board"} component={BoardContainer}/>
+        <Route path={process.env.PUBLIC_URL + "/game/question"} component={QuestionDisplay}/>
       </div>
     </Router>
   )
