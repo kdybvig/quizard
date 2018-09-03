1ad4565c8db5d73bf47f3324926c9bf2cb0fb3d0
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-09-03 15:51:30 -0500

    fixed login and registration

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,21 +1,76 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {fetchUser} from '../actions';
+import {Redirect} from 'react-router-dom';
 
-const LoginPage = (props) => {
-    return (
-        <div class='login-box'>
-            <h1>Login</h1>
-            <h5>Username</h5>
-            <input placeholder="Username"/>
-            <h5>Password</h5>
-            <input placeholder="Password"/>
-            <a id="forgot-password">Forgot password?</a>
-            <button>Sign In</button>
-            <p>Don't have an account? 
-                <Link to={process.env.PUBLIC_URL + '/signup'} > Sign up here.</Link>
-            </p>
-        </div>
-    )
+class LoginPage extends Component {
+    state = {
+        username: '',
+        password: ''
+    }
+
+    handleInputChange = e => {
+        e.preventDefault();
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const user = {
+            username: this.state.username,
+            password: this.state.password
+        }
+        this.setState({
+            username: '',
+            password: ''
+        })
+        this.props.dispatch(fetchUser(user));
+    }
+    render() {
+        
+        if(this.props.user.username !== undefined) {
+            return <Redirect to={process.env.PUBLIC_URL + '/'} />
+        }
+
+        return (
+            <div className='login-box'>
+                <h1>Login</h1>
+                {this.props.user.error && 
+                    <div className="error-message">{this.props.user.error}</div>
+                }
+                <form onSubmit={this.handleSubmit}>
+                <h5>Username</h5>
+                <input 
+                required 
+                onChange={this.handleInputChange} 
+                name ="username" 
+                placeholder="Username"
+                value={this.state.username}
+                />
+                <h5>Password</h5>
+                <input 
+                required 
+                onChange={this.handleInputChange} 
+                type="password" name="password" 
+                placeholder="Password"
+                value={this.state.password}
+                />
+                <a id="forgot-password">Forgot password?</a>
+                <button type="submit">Sign In</button>
+                </form>
+                <p>Don't have an account? 
+                    <Link to={process.env.PUBLIC_URL + '/signup'} > Sign up here.</Link>
+                </p>
+            </div>
+        )
+    }
 }
 
-export default LoginPage;
+const mapStateToProps = state => ({
+    user: state.user
+})
+
+export default connect(mapStateToProps)(LoginPage);
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom';
 
 const SignUp = (props) => {
     return (
-        <div class='login-box'>
+        <div className='login-box'>
             <h1>Register</h1>
             <h5>Username</h5>
-            <input placeholder="Username"/>
+            <input required placeholder="Username"/>
             <h5>Email</h5>
-            <input placeholder="Email"/>
+            <input required placeholder="Email"/>
             <h5>Password</h5>
-            <input placeholder="Password"/>
+            <input required type="password" placeholder="Password"/>
             <h5>Confirm Password</h5>
-            <input placeholder="Password"/>
-            <button>Sign Up</button>
+            <input required type="password" placeholder="Password"/>
+            <button>Sign Up</button> 
             <p>Already have an account? 
                 <Link to={process.env.PUBLIC_URL + '/login'} > Sign in here.</Link>
             </p>
