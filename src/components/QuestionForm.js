78158dedb74fd14506ce7d0ffd2735b85accd993
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-09-15 16:57:07 -0500

    implemented form control

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addQuestions } from '../actions';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Redirect } from 'react-router-dom';
 
-const QuestionForm = (props) => {
+class QuestionForm extends Component {
+  defaultQuestions = () => {
+    return (
+      new Array(5).fill({}).map((question,index) => {
+        return {
+          name: '',
+          answer: '',
+          answered: false,
+          value: (index + 1)*100
+        }
+      })
+    )
+  }
+  state = {
+    questions: this.defaultQuestions()
+  }
 
-  if(!props.categories) props.history.push(process.env.PUBLIC_URL + '/')
-
-  const renderInputs = () => {
+  renderInputs = () => {
     const inputs = [];
     for (let i=0; i<10; i++) {
-      const key = `cat-${i}`
+      const key = `qatext-${i}`
       const questNum = Math.floor(i/2) + 1;
+      const index = questNum -1;
       const className = i%2 === 0 ? 'questContainer' : 'questContainer ansContainer'
-      
-      const label = i%2 === 0 ? 
-        `Question ${questNum} (${questNum*100})` : `Answer` 
-      const input = <textarea cols='50' rows='2' maxLength='250' className='qInput' required/>
+      const isAnswer = i%2 === 0;
+      const type = isAnswer ? 'name' : 'answer'
+      const label = isAnswer ? 
+        `Question ${questNum} (${questNum*100})` : `Answer (Optional)` 
+      const input = (
+        <textarea 
+        cols='50' 
+        rows='2' 
+        index={index}
+        boxtype={type}
+        onChange={this.handleInputChange}
+        maxLength='250' 
+        className='qInput' 
+        value={this.state.questions[index][type]}
+        required/>
+      )
       inputs.push(
         <div className={className} key={key}>
           <h5>{label}</h5>
@@ -27,34 +53,45 @@ const QuestionForm = (props) => {
     return inputs;
   }
 
-  const testSubmit = (e) => {
+  handleInputChange = e => {
     e.preventDefault();
-    const formChildren = Array.from(e.target.childNodes);
-    const questContainerNodes  = formChildren.filter(node => node.className==='questContainer');
-    const questNodes = questContainerNodes.map(container => {
-      const questNode = Array.from(container.childNodes).filter(node => node.className==='qInput')
-      return questNode;
-    });
-    const questions = questNodes.map((node, index) => {
-      return {name: node[0].value, answered: false, value: 100*(index + 1)};
+    console.log(this.state);
+    const index= Number(e.target.getAttribute('index'));
+    const type = e.target.getAttribute('boxtype');
+    const newQuestions = this.state.questions.slice();
+    newQuestions[index][type] = e.target.value;
+    this.setState({
+      questions: newQuestions
     })
-    questNodes.forEach(node => node[0].value='')
-    const isLastCat = props.catIndex === 4
-    props.dispatch(addQuestions(questions));
-    if(isLastCat) props.history.push(process.env.PUBLIC_URL + '/addteams')
   }
 
+  handleSubmit = e => {
+    e.preventDefault();
+    const isLastCat = this.props.catIndex === 4;
+    const questions = this.state.questions.slice();
+    this.props.dispatch(addQuestions(questions));
+    console.log(this.defaultQuestions, 'hello')
+    this.setState({
+      questions: this.defaultQuestions()
+    })
+    if(isLastCat) this.props.history.push(process.env.PUBLIC_URL + '/addteams')
+  }
 
-  return (
-    <div className='form-page'>
-      <form onSubmit={testSubmit}>
-        <h1>{props.title}</h1>
-        <h4>Category: {props.categories[props.catIndex].name}</h4>
-        {renderInputs()}
-        <button type="submit">{props.catIndex === 4 ? 'Save' : 'Next Category'}</button>
-      </form>
-    </div>
-  )
+  render () {
+    if(!this.props.categories.length) {
+       return <Redirect to={process.env.PUBLIC_URL + '/'} />
+    }
+    return (
+      <div className='form-page'>
+        <form onSubmit={this.handleSubmit}>
+          <h1>{this.props.title}</h1>
+          <h4>Category: {this.props.categories[this.props.catIndex].name}</h4>
+          {this.renderInputs()}
+          <button type="submit">{this.props.catIndex === 4 ? 'Save' : 'Next Category'}</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => ({
