7c2330994508fd821b5c72d194c3b3123284d5b5
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-09-15 17:34:22 -0500

    fixed show answer button

--- src/components/QuestionDisplay.js
+++ src/components/QuestionDisplay.js
@@ -1,29 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import TeamSelect from './TeamSelect';
 import { awardPoints } from '../actions';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Redirect } from 'react-router-dom';
 
-const QuestionDisplay = ({activeQ, teams, points, dispatch, history}) => {
-  if(!teams) {
-    history.push(process.env.PUBLIC_URL + '/')
-    return<div></div>
+class QuestionDisplay extends Component {
+  state = {
+    showAnswer: false
   }
 
-  const dispatchAwardPoints = (points, team) => {
-    dispatch(awardPoints(points, team));
-    history.push(process.env.PUBLIC_URL + '/game/board')
+  dispatchAwardPoints = (points, team) => {
+    this.props.dispatch(awardPoints(points, team));
+    this.props.history.push(process.env.PUBLIC_URL + '/game/board')
   }
 
-  return (
-    <div id='questionDisplay'>
-      <h1>
-      {activeQ}
-      </h1>
-      <TeamSelect activeQuestion={activeQ} teams={teams} points={points} awardPoints={dispatchAwardPoints}/>
-      <button id="show-answer">Show Answer</button>
-    </div>
-  )
+  toggleAnswer = () => {
+    const showAnswer = !this.state.showAnswer;
+    this.setState({
+      showAnswer: showAnswer
+    })
+  }
+  render () {
+    if(!this.props.teams) {
+      return<Redirect to={process.env.PUBLIC_URL + '/'} />
+    }
+
+    return (
+      <div id='questionDisplay'>
+        <h1>
+        {this.state.showAnswer ? this.props.answer : this.props.activeQ}
+        </h1>
+        <TeamSelect 
+        activeQuestion={this.props.activeQ} 
+        teams={this.props.teams} 
+        points={this.props.points} awardPoints={this.dispatchAwardPoints}/>
+       {this.props.answer  && <button onClick={this.toggleAnswer} id="show-answer">
+       {this.state.showAnswer ? 'Show Question' : 'Show Answer'}
+       </button>}
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => {
@@ -32,9 +48,12 @@ const mapStateToProps = state => {
       teams: false
     }
   }
+
+  const activeQuestion = state.categories[state.activeQuestion[0]].questions[state.activeQuestion[1]];
   return {
-    activeQ: state.categories[state.activeQuestion[0]].questions[state.activeQuestion[1]].name,
-    points: state.categories[state.activeQuestion[0]].questions[state.activeQuestion[1]].value,
+    activeQ: activeQuestion.name,
+    points: activeQuestion.value,
+    answer: activeQuestion.answer,
     teams: state.teams.map(team => team.name)
   }
 };
