b1140bb11cb98e7569c16287ffd9bf3cc3b93d45
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-09-16 16:33:08 -0500

    updated home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,13 @@ import { Link } from 'react-router-dom';
 const Home = (props) => {
   return (
     <div id='home'>
-      <h1>Welcome to Quizard!</h1>
-      <h4><Link to={process.env.PUBLIC_URL + '/load'}>Load an existing quiz</Link></h4>
-      <h4><Link to={process.env.PUBLIC_URL + '/create'}>Create a new quiz</Link></h4>
+      <div id="main">
+        <h1>Welcome to Quizard!</h1>
+        <ul>
+          <li><Link to={process.env.PUBLIC_URL + '/load'}>Load an existing quiz</Link></li>
+          <li><Link to={process.env.PUBLIC_URL + '/create'}>Create a new quiz</Link></li>
+        </ul>
+      </div>
     </div>
   )
 }
