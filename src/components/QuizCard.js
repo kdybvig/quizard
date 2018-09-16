758304bb2a74c3001ec5756fc4b0c4ce17721976
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-09-16 14:12:42 -0500

    updated quiz card styles

--- src/components/QuizCard.js
+++ src/components/QuizCard.js
@@ -6,6 +6,7 @@ const QuizCard = props => {
     return (
         <div className="quiz-card" onClick={props.handleClick}>
             <h3>{name}</h3>
+            <p><span style={{fontWeight: 'bold'}}>Created by </span><a href='#'>kdybvig</a></p>
             <p>This is a placeholder for the description property.</p>
             {categories.map((category, index) => {
                 return <h5 key={index}>{category.name}</h5>
