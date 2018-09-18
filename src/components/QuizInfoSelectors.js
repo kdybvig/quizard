cedde9554486236a0e5898258d23e1c951813718
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-09-17 20:28:31 -0500

    added QuizInfo and QuizInfoSelectors components

---
+++ src/components/QuizInfoSelectors.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import QuizInfoSelect from './QuizInfoSelect';
+
+const QuizInfoSelectors = props => {
+    const subjectOptions = ['Please select...','Math', 'English', 'Science', 'Social Studies', 'Other'];
+    const gradeLevelOptions = ['Please select...','K-5', '6-8', '9-12', 'Postsecondary', 'Other', 'N/A'];
+    const visibilityOptions = ['Public', 'Private'];
+    const selects = [
+        {name: 'Subject', stateKey: 'subject', options: subjectOptions},
+        {name: 'Grade Level', stateKey: 'grade', options: gradeLevelOptions},
+        {name: 'Visibility', stateKey: 'visibility', options: visibilityOptions}
+    ]
+    return (
+        <div className="quiz-info" id="quiz-info-selects">
+            {selects.map(select => {
+                return (
+                    <div className='info-selector'>
+                        <h6>{select.name}</h6>
+                        <QuizInfoSelect 
+                        options={select.options} 
+                        stateKey={select.stateKey}
+                        />
+                    </div>
+                )
+                
+
+            })}
+        </div>
+    )
+}
+
+export default QuizInfoSelectors
