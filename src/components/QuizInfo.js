7aa08159fb7b99715aff7c5b91ed49cfb8d33002
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-09-22 11:07:17 -0500

    reformatted quiz info

--- src/components/QuizInfo.js
+++ src/components/QuizInfo.js
@@ -4,8 +4,8 @@ const QuizInfo = props => {
     const {subject, gradeLevel} = props.info;
     return (
         <div id="quiz-info">
-            <h6><b>Subject</b> {subject}</h6>
-            <h6><b>Grade Level</b> {gradeLevel}</h6>
+            <h6><b>Subject:</b> {subject ? subject : 'N/A'}</h6>
+            <h6><b>Grade Level:</b> {gradeLevel? gradeLevel : 'N/A'}</h6>
         </div>
   )
 }
