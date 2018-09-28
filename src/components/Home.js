1e1cb10f93c9bd97327d9576033d066a444c0dd2
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-09-28 16:56:47 -0500

    modified home page appearance

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,10 @@ const Home = (props) => {
   return (
     <div id='home'>
       <div id="main">
-        <h1>Welcome to Quizard!</h1>
+        <h2>Welcome to</h2>
+        <h1>Quizard</h1>
         <ul>
-          <li><Link to={process.env.PUBLIC_URL + '/load'}>Load an existing quiz</Link></li>
+          <li><Link to={process.env.PUBLIC_URL + '/load'}>Explore public quizzes</Link></li>
           <li><Link to={process.env.PUBLIC_URL + props.linkTwo}>{props.linkTwoText}</Link></li>
         </ul>
       </div>
