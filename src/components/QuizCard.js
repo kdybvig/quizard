aef5ab4ee667801441a0e58cd1cac39fb0a807ab
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-10-01 16:27:57 -0500

    modified quiz card to show teams for in-progress games

--- src/components/QuizCard.js
+++ src/components/QuizCard.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 const QuizCard = props => {
-    const {name, categories, description} = props.game
+    const {name, categories, description, teams} = props.game
     return (
         <div className="quiz-card" onClick={props.handleClick}>
             <h3>{name}</h3>
             <p><span style={{fontWeight: 'bold'}}>Created by </span><a href='#'>kdybvig</a></p>
             <p>{description}</p>
+            {teams.map((team, index) => {
+                return <h5 style={{backgroundColor: 'black', textAlign: 'left', borderColor: 'black'}} key={'team-' + index}>{team.name}: {team.points}</h5>
+            })}
             {categories.map((category, index) => {
                 return <h5 key={index}>{category.name}</h5>
             })}
