3e014c594effbcca253413894c1db6f4fcbc7d69
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-10-02 19:20:18 -0500

    updated quiz card to display the correct creator of the quiz

--- src/components/QuizCard.js
+++ src/components/QuizCard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const QuizCard = props => {
-    const {name, categories, description, teams} = props.game
+    const {name, categories, description, teams, createdBy} = props.game
     return (
         <div className="quiz-card" onClick={props.handleClick}>
             <h3>{name}</h3>
-            <p><span style={{fontWeight: 'bold'}}>Created by </span><a href='#'>kdybvig</a></p>
+            <p><span style={{fontWeight: 'bold'}}>Created by </span><a href='#'>{createdBy}</a></p>
             <p>{description}</p>
             {teams.map((team, index) => {
                 return <h5 style={{backgroundColor: 'black', textAlign: 'left', borderColor: 'black'}} key={'team-' + index}>{team.name}: {team.points}</h5>
